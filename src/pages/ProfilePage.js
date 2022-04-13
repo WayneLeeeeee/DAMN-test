6b3447b8fc3b78a471c5b0aae2bab1372fb5ac17
Jim <jimmy1245966@gmail.com>
2022-04-13 21:57:56 +0800

    經驗條

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,41 +1,49 @@
-import React from "react";
-import BottomNav from "../components/BottomNav";
-import { useState } from "react";
-import { getApps, initializeApp } from "firebase/app";
-import { getAuth, signOut } from "firebase/auth";
-import { firebaseConfig } from "../../src/firebase";
-import { useStateValue } from "../../src/StateProvider";
-import { actionTypes } from "../../src/reducer";
-import { useNavigate } from "react-router-dom";
-import { styled } from "@mui/material/styles";
-import Box from "@mui/material/Box";
-import Paper from "@mui/material/Paper";
-import Grid from "@mui/material/Grid";
-import ImageList from "@mui/material/ImageList";
-import ImageListItem from "@mui/material/ImageListItem";
-import Divider from "@mui/material/Divider";
-import Container from "@mui/material/Container";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Typography from "@mui/material/Typography";
+import React from 'react';
+import BottomNav from '../components/BottomNav';
+import { useState, useEffect } from 'react';
+import { getApps, initializeApp } from 'firebase/app';
+import { getAuth, signOut } from 'firebase/auth';
+import { firebaseConfig } from '../../src/firebase';
+import { useStateValue } from '../../src/StateProvider';
+import { actionTypes } from '../../src/reducer';
+import { useNavigate } from 'react-router-dom';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import Divider from '@mui/material/Divider';
+import Container from '@mui/material/Container';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
 import {
   Button,
   CardActionArea,
   CardActions,
   FormControlLabel,
   Switch,
-} from "@mui/material";
-import Avatar from "@mui/material/Avatar";
-import profile from "../images/profile.jpg";
-import { margin } from "@mui/system";
-import { Padding } from "@material-ui/icons";
-import { Line, Circle } from "rc-progress";
-import Tabs from "@mui/material/Tabs";
-import Tab from "@mui/material/Tab";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import SettingsIcon from "@mui/icons-material/Settings";
-import EditIcon from "@mui/icons-material/Edit";
+} from '@mui/material';
+import Avatar from '@mui/material/Avatar';
+import profile from '../images/profile.jpg';
+import { margin } from '@mui/system';
+import { Padding } from '@material-ui/icons';
+import { Line, Circle } from 'rc-progress';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import SettingsIcon from '@mui/icons-material/Settings';
+import EditIcon from '@mui/icons-material/Edit';
+import { db, auth } from '../firebase';
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  doc,
+  getDoc,
+} from 'firebase/firestore';
 
 const ProfilePage = () => {
   const [{ user, isSTTFromMicOpen }, dispatch] = useStateValue();
@@ -43,7 +51,7 @@ const ProfilePage = () => {
     initializeApp(firebaseConfig);
   }
   const navigate = useNavigate();
-  const [message, setMessage] = useState("");
+  const [message, setMessage] = useState('');
   // const [value, setValue] = React.useState(0);
   // const handleChange = (event, newValue) => {
   //   setValue(newValue);
@@ -63,77 +71,77 @@ const ProfilePage = () => {
     try {
       const auth = getAuth();
       await signOut(auth);
-      setMessage("");
+      setMessage('');
       dispatch({
         type: actionTypes.SET_USER,
         user: null,
       });
-      localStorage.removeItem("userUid");
-      navigate("/");
-      console.log("已登出");
+      localStorage.removeItem('userUid');
+      navigate('/');
+      console.log('已登出');
     } catch (error) {
-      setMessage("" + error);
+      setMessage('' + error);
     }
   };
 
   console.log(user);
 
   const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
     ...theme.typography.body2,
     padding: theme.spacing(1),
-    textAlign: "center",
+    textAlign: 'center',
     color: theme.palette.text.secondary,
   }));
 
   const itemData = [
     {
-      img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
-      title: "Breakfast",
+      img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
+      title: 'Breakfast',
     },
     {
-      img: "https://images.unsplash.com/photo-1551782450-a2132b4ba21d",
-      title: "Burger",
+      img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
+      title: 'Burger',
     },
     {
-      img: "https://images.unsplash.com/photo-1522770179533-24471fcdba45",
-      title: "Camera",
+      img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
+      title: 'Camera',
     },
     {
-      img: "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c",
-      title: "Coffee",
+      img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
+      title: 'Coffee',
     },
     {
-      img: "https://images.unsplash.com/photo-1533827432537-70133748f5c8",
-      title: "Hats",
+      img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
+      title: 'Hats',
     },
     {
-      img: "https://images.unsplash.com/photo-1558642452-9d2a7deb7f62",
-      title: "Honey",
+      img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
+      title: 'Honey',
     },
     {
-      img: "https://images.unsplash.com/photo-1516802273409-68526ee1bdd6",
-      title: "Basketball",
+      img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
+      title: 'Basketball',
     },
     {
-      img: "https://images.unsplash.com/photo-1518756131217-31eb79b20e8f",
-      title: "Fern",
+      img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
+      title: 'Fern',
     },
     {
-      img: "https://images.unsplash.com/photo-1597645587822-e99fa5d45d25",
-      title: "Mushrooms",
+      img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
+      title: 'Mushrooms',
     },
     {
-      img: "https://images.unsplash.com/photo-1567306301408-9b74779a11af",
-      title: "Tomato basil",
+      img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
+      title: 'Tomato basil',
     },
     {
-      img: "https://images.unsplash.com/photo-1471357674240-e1a485acb3e1",
-      title: "Sea star",
+      img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
+      title: 'Sea star',
     },
     {
-      img: "https://images.unsplash.com/photo-1589118949245-7d38baf380d6",
-      title: "Bike",
+      img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
+      title: 'Bike',
     },
   ];
 
@@ -143,20 +151,53 @@ const ProfilePage = () => {
         type: actionTypes.SET_IS_STT_FROM_MIC_OPEN,
         isSTTFromMicOpen: false,
       });
-    }else{
-       dispatch({
-         type: actionTypes.SET_IS_STT_FROM_MIC_OPEN,
-         isSTTFromMicOpen: true,
-       });
+    } else {
+      dispatch({
+        type: actionTypes.SET_IS_STT_FROM_MIC_OPEN,
+        isSTTFromMicOpen: true,
+      });
     }
   };
 
+  const [users, setUsers] = useState([]);
+
+  console.log(users);
+
+  const userUid = localStorage.getItem('userUid');
+  useEffect(() => {
+    async function readData() {
+      const docRef = doc(db, 'users', `${userUid}`);
+      const docSnap = await getDoc(docRef);
+      const temp = [];
+
+      if (docSnap.exists()) {
+        console.log('Document data:', docSnap.data());
+        temp.push({ ...docSnap.data() });
+        console.log(temp);
+        setUsers([...temp]);
+      } else {
+        // doc.data() will be undefined in this case
+        console.log('No such document!');
+      }
+    }
+    readData();
+  }, [db]);
+  const percent = users.map((number) => number);
+
+  console.log(percent[0]);
+
   return (
-    <div className="ProfilePage" sx={{ height: "100vh" }}>
-      <Card className="profile__card" sx={{ maxWidth: 500, height: 300 }}>
-        <div className="profile__progress">
-          <Circle percent="87" strokeWidth="6" strokeColor="#FFD700" />
-        </div>
+    <div className="ProfilePage">
+      <Card className="profile__card" sx={{ height: 400 }}>
+        {users.map((testuser) => (
+          <div className="profile__progress">
+            <Circle
+              percent={testuser.progress}
+              strokeWidth="6"
+              strokeColor="#FFD700"
+            />
+          </div>
+        ))}
         <Avatar className="profile__avatar" img src={profile} alt="Logo" />
         <Typography className="profile__name">Apple</Typography>
         <Button className="profile__button">
@@ -187,11 +228,11 @@ const ProfilePage = () => {
       <TabPanel value={value} index={0}>
         <Divider />
         <ImageList
-          sx={{ width: 420, height: 400, marginLeft: -3 }}
           className="profile__imagelist"
           cols={3}
           rowHeight={140}
           gap={0}
+          sx={{ height: 400 }}
         >
           {itemData.map((item) => (
             <ImageListItem key={item.img}>
@@ -209,11 +250,11 @@ const ProfilePage = () => {
       <TabPanel value={value} index={1}>
         <Divider />
         <div className="profile__tabpanel">
-          <Container sx={{ maxWidth: 500, height: 350 }}>
+          <Container sx={{ height: 320 }}>
             <Typography
               sx={{
                 fontSize: 26,
-                fontWeight: "bold",
+                fontWeight: 'bold',
                 marginTop: 3,
               }}
             >
@@ -226,14 +267,14 @@ const ProfilePage = () => {
             >
               Apple
               <Button>
-                <EditIcon fontSize="small" sx={{ marginLeft: 31 }}></EditIcon>
+                <EditIcon fontSize="small" sx={{ marginLeft: 30.5 }}></EditIcon>
               </Button>
             </Typography>
 
             <Typography
               sx={{
                 fontSize: 26,
-                fontWeight: "bold",
+                fontWeight: 'bold',
                 marginTop: 2,
               }}
             >
@@ -246,14 +287,14 @@ const ProfilePage = () => {
             >
               apple@gmail.com
               <Button>
-                <EditIcon fontSize="small" sx={{ marginLeft: 18 }}></EditIcon>
+                <EditIcon fontSize="small" sx={{ marginLeft: 17.8 }}></EditIcon>
               </Button>
             </Typography>
 
             <Typography
               sx={{
                 fontSize: 26,
-                fontWeight: "bold",
+                fontWeight: 'bold',
                 marginTop: 3,
               }}
             >
@@ -268,7 +309,7 @@ const ProfilePage = () => {
               <Button>
                 <EditIcon
                   fontSize="small"
-                  sx={{ marginLeft: 26, marginTop: -1 }}
+                  sx={{ marginLeft: 27, marginTop: -1 }}
                 ></EditIcon>
               </Button>
             </Typography>
@@ -276,7 +317,7 @@ const ProfilePage = () => {
             <Typography
               sx={{
                 fontSize: 26,
-                fontWeight: "bold",
+                fontWeight: 'bold',
                 marginTop: 3,
               }}
             >
@@ -295,7 +336,6 @@ const ProfilePage = () => {
                 ></EditIcon>
               </Button>
             </Typography>
-
             {/* 關閉小當家按鈕 */}
             <FormControlLabel
               control={
@@ -306,7 +346,6 @@ const ProfilePage = () => {
               }
               label="關閉小當家"
             />
-
             <form>
               <Button
                 className="profile__logout"
@@ -317,7 +356,6 @@ const ProfilePage = () => {
                 登出
               </Button>
               {message}
-              <br />
             </form>
           </Container>
         </div>
--- src/pages/recipe/RecipeItemPage.js
+++ src/pages/recipe/RecipeItemPage.js
@@ -1,35 +1,43 @@
-import React, { useEffect, useState } from "react";
-import Tabs from "./Tabs";
-import { useNavigate, useParams } from "react-router-dom";
-import { doc, getDoc } from "firebase/firestore";
-import { db } from "../../firebase";
-import { Box, ThemeProvider } from "@mui/system";
-import { Paper } from "@mui/material";
-import theme from "../../function/theme";
-import ImageIcon from "@mui/icons-material/Image";
-import ImageStepper from "../../components/ImageStepper";
-import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
-import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
-import CheckCircleIcon from "@mui/icons-material/CheckCircle";
-import DinnerDiningIcon from "@mui/icons-material/DinnerDining";
+import React, { useEffect, useState } from 'react';
+import Tabs from './Tabs';
+import { useNavigate, useParams } from 'react-router-dom';
+import { doc, getDoc } from 'firebase/firestore';
+import { db } from '../../firebase';
+import { Box, ThemeProvider } from '@mui/system';
+import { Paper } from '@mui/material';
+import theme from '../../function/theme';
+import ImageIcon from '@mui/icons-material/Image';
+import ImageStepper from '../../components/ImageStepper';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import DinnerDiningIcon from '@mui/icons-material/DinnerDining';
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+  updateDoc,
+  increment,
+} from 'firebase/firestore';
 
 function RecipeItem({ propsData }) {
   const [data, setData] = useState(null);
   const [iscompleted, setIscompleted] = useState(false);
   let params = useParams();
   let navigate = useNavigate();
-  const handleGoBackToHomePage = () => navigate("/");
+  const handleGoBackToHomePage = () => navigate('/');
 
   // fetch recipe detail data from fireStore
   const fetchData = async () => {
-    const docRef = doc(db, "recipes", params.id);
+    const docRef = doc(db, 'recipes', params.id);
     const docSnap = await getDoc(docRef);
     if (docSnap.exists()) {
-      console.log("Document data:", docSnap.data());
+      console.log('Document data:', docSnap.data());
       setData(docSnap.data());
     } else {
       // doc.data() will be undefined in this case
-      console.log("No such document!");
+      console.log('No such document!');
     }
   };
 
@@ -49,20 +57,60 @@ function RecipeItem({ propsData }) {
     }
   }, []);
 
+  // const Rating = async () => {
+  //   const docRef = collection(db, 'recipes');
+  //   const temp = [];
+  //   const docSnap = await getDocs(docRef);
+  //   docSnap.forEach((doc) => {
+  //     temp.push({ rating: doc.data().rating });
+  //   });
+  //   console.log(temp);
+  // };
+  // Rating();
+
+  const [rating, setRating] = useState([]);
+  console.log(rating);
+
+  // useEffect(() => {
+  //   async function Rating() {
+  //     const querySnapshot = await getDocs(collection(db, 'recipes'));
+  //     const temp = [];
+  //     querySnapshot.forEach((doc) => {
+  //       temp.push({ rating: doc.data().rating });
+  //     });
+  //     console.log(temp);
+  //     setRating([...temp]);
+  //   }
+  //   console.log(rating);
+  //   Rating();
+  // }, [db]);
+
+  const userUid = localStorage.getItem('userUid');
+  const update = async function () {
+    const db = getFirestore();
+    try {
+      await updateDoc(doc(db, 'users', `${userUid}`), {
+        progress: increment(5),
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return (
     <ThemeProvider theme={theme}>
       <div className="recipeItem__title">
         <ArrowBackIosIcon
-          sx={{ color: "#ffffff" }}
+          sx={{ color: '#ffffff' }}
           onClick={handleGoBackToHomePage}
         />
         <h3>{data?.name}</h3>
-        <DinnerDiningIcon sx={{ paddingLeft: "10px", fontSize: "32px" }} />
+        <DinnerDiningIcon sx={{ paddingLeft: '10px', fontSize: '32px' }} />
       </div>
       <Paper
         elevation={3}
         className="recipeItem__container"
-        sx={{ color: "text.normal" }}
+        sx={{ color: 'text.normal' }}
       >
         <div className="recipeItem__wrap">
           {data?.thumbnail?.url && <ImageStepper data={data} />}
@@ -75,14 +123,82 @@ function RecipeItem({ propsData }) {
       </Paper>
       {iscompleted ? (
         <div className="recipeItem__checkIcon">
-          <CheckCircleIcon sx={{ color: "green" }} />
+          <CheckCircleIcon sx={{ color: 'green' }} />
           <h4>已完成</h4>
         </div>
       ) : (
         <div className="recipeItem__checkIcon">
           <CheckCircleOutlineIcon
-            sx={{ color: "red" }}
-            onClick={handleComplete}
+            sx={{ color: 'red' }}
+            onClick={() => {
+              if (data?.rating == 0.5) {
+                update();
+              } else if (data?.rating == 1) {
+                update();
+                update();
+              } else if (data?.rating == 1.5) {
+                update();
+                update();
+                update();
+              } else if (data?.rating == 2) {
+                update();
+                update();
+                update();
+                update();
+              } else if (data?.rating == 2.5) {
+                update();
+                update();
+                update();
+                update();
+                update();
+              } else if (data?.rating == 3) {
+                update();
+                update();
+                update();
+                update();
+                update();
+                update();
+              } else if (data?.rating == 3.5) {
+                update();
+                update();
+                update();
+                update();
+                update();
+                update();
+                update();
+              } else if (data?.rating == 4) {
+                update();
+                update();
+                update();
+                update();
+                update();
+                update();
+                update();
+                update();
+              } else if (data?.rating == 4.5) {
+                update();
+                update();
+                update();
+                update();
+                update();
+                update();
+                update();
+                update();
+                update();
+              } else if (data?.rating == 5) {
+                update();
+                update();
+                update();
+                update();
+                update();
+                update();
+                update();
+                update();
+                update();
+                update();
+              }
+              handleComplete();
+            }}
           />
           <h4>未完成</h4>
         </div>
